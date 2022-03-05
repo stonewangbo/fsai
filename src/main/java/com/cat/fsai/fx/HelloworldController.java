814eee2c188ef6ce177767bd3fab72d9c129fb09
stone <stone.wangbo@gmail.com>
2022-03-05 16:36:20 +0800

    可运行的fx版本

--- src/main/java/com/cat/fsai/fx/HelloworldController.java
+++ src/main/java/com/cat/fsai/fx/HelloworldController.java
@@ -2,24 +2,32 @@ package com.cat.fsai.fx;
 
 import com.cat.fsai.cc.binance.BinanceMarket;
 import com.cat.fsai.type.TR;
-import de.felixroske.jfxsupport.FXMLController;
-import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.concurrent.CountDownLatch;
 
-@FXMLController
+
+@Component
 @Slf4j
-public class HelloworldController {
+public class HelloworldController implements Initializable {
 
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private LineChart lineA;
 
     @FXML
     private TextField nameField;
@@ -28,14 +36,22 @@ public class HelloworldController {
     @Autowired
     private BinanceMarket binanceMarket;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        welcomeText.setText("初始化");
+    }
+
     @FXML
     private void setHelloText(final Event event) {
         CountDownLatch downLatch = new CountDownLatch(1);
         binanceMarket.depth((dg, e)->{
-            log.info("dg:{}",dg);
+            Platform.runLater(()->{
+                log.info("币安最新交易深度:{}",dg);
+                welcomeText.setText("dg:"+dg);
+            });
             downLatch.countDown();
         }, TR.BTC_USDT);
 
-        welcomeText.setText("12343");
+        welcomeText.setText("测试：");
     }
 }
